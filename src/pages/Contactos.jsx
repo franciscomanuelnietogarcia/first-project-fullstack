d5cc92806992c764692d8a3b7f60a3c131f55c21
Celia Gil Prieto <Gilprietoceliia@gmail.com>
2023-09-10 19:50:50 +0200

    cambios

--- src/pages/Contactos.jsx
+++ src/pages/Contactos.jsx
@@ -5,7 +5,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
-import "./Contactos.css"
+import Pages from "../pages/Contactos/Contactos";
 
 
 const Contactos = () => {
