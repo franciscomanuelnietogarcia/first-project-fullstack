9301b6e0fdbd0ef15eb07f184e6f74eab08b25c3
Rodrigo <ramunozg@gmail.com>
2023-09-14 17:32:43 +0200

    test

---
+++ src/components/CoursCard/CoursCard.test.jsx
@@ -0,0 +1,9 @@
+// import { render, screen } from '@testing-library/react';
+// import CoursCard from './CoursCard';
+
+
+//  test('render CoursCard', () => {
+//     render(<CoursCard/>);
+//     const functionBotton = screen.(/onClick/i);
+//     expect(functionBotton).toBeDefined();
+//  })
